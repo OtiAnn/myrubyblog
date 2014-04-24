96133b82816d2f9e9893daf1d4c612ac00b9d195
Annie Chen <anniebh30@gmail.com>
2014-04-24 17:47:59 +0800

    Controller,Views Practice

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = "This is 1st sample"
+		@content_sencond = "This is 2nd sample"
+	end
+
+	def new
+		
+	end
+	
+	def create
+		
+	end
+	
+	def edit
+		
+	end
+	
+	def update
+		
+	end
+	
+	def show
+		
+	end
+	
+	def destroy
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Myrubyblog::Application.routes.draw do
+  root "home#index"
+    resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root to: redirect('/homes')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
