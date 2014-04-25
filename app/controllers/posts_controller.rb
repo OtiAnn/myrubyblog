9970e8d99230a8d349166ec995b2c4a95ba408f8
Annie Chen <anniebh30@gmail.com>
2014-04-25 17:28:48 +0800

    Add New Post Page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,17 @@ class PostsController < ApplicationController
 	end
 
 	def new
-		
+		@post = Post.new
+		@category = Category.all
 	end
 	
 	def create
-		
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path, :notice => "Your post has been saved!"
+		else
+			render "new"
+		end
 	end
 	
 	def edit
